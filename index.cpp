ed64c4e1107832278b08a05754f669c9f3d963f2
Pafku <91912211+Pafku@users.noreply.github.com>
2023-02-17 11:53:07 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -2,21 +2,21 @@
 
 int minimax(int board[], int depth, bool isMax, int nodeIndex, int h){
     if (depth == h){
-        std::cout << "<><> " << nodeIndex << "   " << board[nodeIndex] << "   \n";
+        // std::cout << "<><> " << nodeIndex << "   " << board[nodeIndex] << "   \n";
         return board[nodeIndex];
-    } else{
-        std::cout << "\nd: " << depth << "  n: " << nodeIndex*2 << "  1: " << nodeIndex*2 + 1 << "\n";
-    }
+    } //else{
+    //     std::cout << "\nd: " << depth << "  n: " << nodeIndex*2 << "  1: " << nodeIndex*2 + 1 << "\n";
+    // }
 
     if(isMax){
         int tell = std::max(minimax(board, depth+1, false, nodeIndex*2, h), 
             minimax(board, depth+1, false, nodeIndex*2 + 1, h));
-        std::cout << tell << " < Max " << depth << "\n";
+        // std::cout << tell << " < Max " << depth << "\n";
         return tell;
     } else{
         int tell = std::min(minimax(board, depth+1, true, nodeIndex*2, h), 
             minimax(board, depth+1, true, nodeIndex*2 + 1, h));
-        std::cout << tell << " < Min " << depth << "\n";
+        // std::cout << tell << " < Min " << depth << "\n";
         return tell;
     }
 }
